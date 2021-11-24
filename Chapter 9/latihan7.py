2e029dc817fccc3b508c48ed4928d90323159b00
mutiahratih <mutiaratih77@gmail.com>
2021-11-24 08:19:07 +0700

    Update latihan7.py

--- Chapter 9/latihan7.py
+++ Chapter 9/latihan7.py
@@ -2,18 +2,17 @@ mhs = ['K001:ROSIHAN ARI:1979-09-01:SOLO',
        'K002:DWI AMALIA FITRIANI:1979-09-17:KUDUS',
        'K003:FAZA FAUZAN:2005-01-28:KARANGANYAR']
 
-print('================================================================')
-print('NIM     NAMA MAHASISWA           TGL LAHIR      TEMPAT LAHIR    ')
-print('----------------------------------------------------------------')
+print('=================================================================')
+print('NIM     NAMA MAHASISWA           TGL LAHIR       TEMPAT LAHIR    ')
+print('-----------------------------------------------------------------')
 
 for i in mhs:
     pecah = i.split(':')
     print(pecah[0].ljust(8), end='')
     print(pecah[1].ljust(25), end='')
     tanggal = pecah[2].split('-')
-    urut = tanggal[2],tanggal[1],tanggal[0]
-    tgl = '/'.join(urut)
-    print(tgl.ljust(16), end='')
+    urut = '/'.join([tanggal[2],tanggal[1],tanggal[0]])
+    print(urut.ljust(16), end='')
     print(pecah[3].ljust(16))
 
-print('----------------------------------------------------------------')
+print('-----------------------------------------------------------------')
